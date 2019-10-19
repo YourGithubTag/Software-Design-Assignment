4c0b7b3a441be98ce7e0346326e123ce19e4eaa6
Nikhil Kumar <nickkumar33@gmail.com>
2019-10-20 03:03:30 +1300

    initial commit, with linked list functions mostly completed, sort, or lane changing has not been implemented

---
+++ Platoon.cpp
@@ -0,0 +1,132 @@
+#include "Platoon.h"
+
+Platoon::Platoon(string init)
+{
+    // the string 'init' looks like:
+    // a,b ; c,d ; ... where, a and c are the vehicle id, b and d are positions.
+    stringstream ss(init);
+	string segment;
+	while(getline(ss, segment, ';'))
+	{
+		size_t found = segment.find(",");
+		if (found!=std::string::npos){
+			string id = segment.substr(0, found);
+			string pos = segment.substr(found+1, segment.size()-1);
+
+			// conversion to integer
+			stringstream s1(id);
+			int id_value;
+			s1 >> id_value;
+			stringstream s2(pos);
+			int pos_value;
+			s2 >> pos_value;
+			Car* car = new Car(id_value, pos_value);
+
+            if (head == NULL)
+            {
+                head = car;
+                tail = car;
+            }
+            else
+            {
+                tail->set_next(car);
+                car->set_prev(tail);
+                tail = car;
+                car->set_next(NULL);
+            }
+		}
+	}
+}
+
+//TODO comments
+void Platoon::remove(Car* c) {
+	Car* previousCar = c->get_prev();
+	Car* nextCar = c->get_next();
+
+	if (nextCar == NULL) {
+		previousCar->set_next(nextCar);
+		tail = previousCar;
+		c->set_next(NULL);
+		c->set_prev(NULL);
+	}
+	else if (previousCar == NULL) {
+		nextCar->set_next(previousCar);
+		head = nextCar;
+		c->set_next(NULL);
+		c->set_prev(NULL);
+	}
+	else {
+		previousCar->set_next(nextCar);
+		nextCar->set_prev(previousCar);
+
+		c->set_next(NULL);
+		c->set_prev(NULL);
+	}
+}
+void Platoon::append(Car* c){
+	if (head == NULL && tail == NULL) {
+		tail = c;
+		head=c;
+	}
+
+	Car* last = Platoon::get_tail();
+	last->set_next(c);
+	c->set_prev(last);
+	c->set_next(NULL);
+	tail = c;
+
+}
+void Platoon::prepend(Car* c){
+	Car* first = Platoon::get_head();
+		first->set_prev(c);
+		c->set_next(first);
+		c->set_prev(NULL);
+		head = c;
+}
+
+void Platoon::insert(Car* c){
+	bool inserted = false;
+	Car* p = head;
+	while(!inserted)
+	{
+		//TODO MAybe have a check for head and tail
+		if ((c->get_position() < p->get_next()->get_position() ) && (p->get_position() < c->get_position()) ) {
+
+			c->set_next(p->get_next());
+			c->set_prev(p);
+
+			p->get_next()->set_prev(c);
+			p->set_next(c);
+			inserted = true;
+
+		} else {
+			p = p->get_next();
+		}
+	}
+}
+
+void Platoon::sort() {
+	/*Car* c = head;
+	Car* d = head;
+	bool swapped = false;
+	   while(c != NULL){
+		   while(d->get_next() != NULL){
+				if (d->get_position() < d->get_next()->get_position() ){
+					swap(d,d->get_next());
+				}
+	    }
+
+}*/
+}
+
+
+Car* Platoon::get_tail()
+{
+	return tail;
+}
+
+Car* Platoon::get_head()
+{
+	return head;
+}
+
---
+++ TrafficModel.cpp
@@ -0,0 +1,119 @@
+#include "TrafficModel.h"
+
+TrafficModel::TrafficModel() { }
+TrafficModel::~TrafficModel(){ }
+
+void TrafficModel::set_commands(vector<string> commands)
+{
+	this->commands = commands;
+}
+
+/* A helper function. 
+ * Given a specific id as the input, it searches the lane change command stored in
+ * "commands". Returns the value of the turn-light in the command for this car id.
+ * 0 = no light, 1 = left light, 2 = right light.
+ */
+int TrafficModel::get_lane_change_command(int id)
+{
+	int count = commands.size();
+	for (int i = 0; i < count; i++){
+		size_t found = commands[i].find(',');
+		string iter_id = commands[i].substr(0, found);
+		string light = commands[i].substr(found+1, commands[i].size()-1);
+		stringstream ss1(iter_id);
+		int id_value;
+		ss1 >> id_value;
+		if (id_value == id){
+			stringstream ss2(light);
+			int light_value;
+			ss2 >> light_value;
+			return light_value;
+		}
+	}
+	return 0;
+}
+
+/*
+ * The function that updates the vehicle positions and states.
+ */
+void TrafficModel::update()
+{
+	// TODO: complete this function
+	for (int i = 0; i < platoons.size(); i++){
+		Car* iterate = platoons[i].get_tail();
+		while (iterate != NULL){
+
+			/*for (int j = 0; j < sizeof(commands); j++ ) { //TODO change structure of this loop
+
+				//string com = commands[j];
+				//com = SimulationUI::split(com);
+
+				if (com[2] == iterate->get_id() ) { // check how to get car ID from Command
+					if (Validcheck() == 1){
+						platoons[i].remove(iterate);
+						platoons[i-1].insert(iterate); //TODO tree up  insert
+					}
+					if (Validcheck() == 2){
+						platoons[i].remove(iterate);
+					    platoons[i+1].insert(iterate); //TODO  tree  down insert
+					}
+				}
+				else {
+
+					}
+			}*/
+
+			int nextpos = iterate->get_position() +1 ;
+			iterate->set_position(nextpos);
+			//moving backwards
+			iterate = iterate->get_prev();
+		}
+	}
+}
+
+int TrafficModel::Validcheck() {
+
+//TODO if statement to check if turning left into out of bounds
+
+//TODO if statement to check if turning right into out of bounds
+
+//TODO if statement to check the above or below lane and if there is a position is free
+
+	//TODO if below
+	//TODO if above
+	return 0;
+
+}
+
+/*
+ * Initialization based on the input information
+ */
+void TrafficModel::initialize(vector<string> info)
+{
+	int lane_count = info.size();
+	for (int i = 0; i < lane_count; i++){
+		Platoon p = Platoon(info[i]);
+		platoons.push_back(p);
+	}
+}
+
+// Returns all the vehicle states in the system
+vector<string> TrafficModel::get_system_state()
+{
+	vector<string> output;
+	int size = platoons.size();
+	for (int i = 0; i < size; i++){
+		// get the last vehicle in the platoon
+		Car* temp = platoons[i].get_tail();
+		string s = "";
+		ostringstream out;
+		while (temp != NULL){
+			out << ";(" << temp->get_id() << "," << i << "," << temp->get_position() << \
+					 "," << get_lane_change_command(temp->get_id()) << ")";
+			temp = temp->get_prev();
+		}
+
+		output.push_back(out.str());
+	}
+	return output;
+}
